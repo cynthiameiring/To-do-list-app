00ba99f09909409d3c3c5c9a7245b78e53776d9a
cynthiameiring <cynthia.meiring@hotmail.com>
2020-02-12 16:10:32 +0100

    router for creating/posting tasks

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -4,7 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Annotation\Request;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Task;
 
 class ToDoListController extends AbstractController
 {
@@ -19,10 +20,22 @@ class ToDoListController extends AbstractController
         /**
      * @Route("/create", name="create_task", methods={"POST"})
      */
-    public function create()
+    public function create(Request $request)
     {
-        
+        $title = trim($request->request->get('title'));
+        if(empty($title))
+        return $this->redirectToRoute('to_do_list');
+
+        $entityManger = $this->getDoctrine()->getManager();
+
+        $task = new Task;
+        $task->setTitle($title);
+        $entityManger->persist($task);
+        $entityManger->flush();     
+
+        return $this->redirectToRoute('to_do_list');
     }
+
 
         /**
      * @Route("/switch-status/{id}", name="switch-status")
